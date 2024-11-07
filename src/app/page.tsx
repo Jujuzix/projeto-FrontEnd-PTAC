ec8631d4238a69d99525da6e27373fd25af3de1c
AsGhrK <asghrkcasari@gmail.com>
2024-11-07 09:22:12 -0400

    07/11/2024

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import styles from "./styles/page.module.css"; 
+import styles from "./styles/page.module.css";
 import Botao from "./components/Botao";
 
 export default function Home() {
@@ -11,9 +11,14 @@ export default function Home() {
   if (user) {
     return (
       <div className={styles.page}>
-        <h1 className={styles.titulo}>Bem-Vindo à Página Principal</h1>
-        <Botao titulo="Ir para Página de Login" botao={() => route.push('/paginaLogin')}/>
-        <Botao titulo="Faça o seu Cadastro" botao={()=> route.push('/paginaCadastro')}/>
+        <h1 className={styles.titulo}>Bem-Vindo ao Restaurante!</h1>
+        <p className={styles.mensagem}>
+          Estamos felizes em tê-lo aqui. Escolha uma das opções abaixo para continuar.
+        </p>
+        <div className={styles.botoesContainer}>
+          <Botao titulo="Ir para Página de Login" botao={() => route.push('/paginaLogin')} />
+          <Botao titulo="Faça o seu Cadastro" botao={() => route.push('/paginaCadastro')} />
+        </div>
       </div>
     );
   }
