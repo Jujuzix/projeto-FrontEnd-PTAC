6e9676452e38c6c5736ee3abb2ec554024077c05
Ju Maronez <maronez.juliana@gmail.com>
2025-01-21 11:04:52 -0400

    Comentarios para documentação do código part4

--- src/app/components/Botao.tsx
+++ src/app/components/Botao.tsx
@@ -1,10 +1,13 @@
+// estilização do botão
 import styles from "../styles/login.module.css"; 
 
+//definição para o tipo de propriedades que o componente irá receber
 type BotaoProp = {
     titulo: string,
-    botao: () => void
+    botao: () => void //função callback executada para clicar no botão
 }
 
+// declaração de componente utilizando React.FC para tipagem
 const Botao: React.FC<BotaoProp> = ({titulo, botao}) => {
     return (
         <button className={styles.botao} onClick={botao}>
@@ -13,4 +16,5 @@ const Botao: React.FC<BotaoProp> = ({titulo, botao}) => {
     );
 }
 
+//exportação do componente para reutilização em outros arquivos do projeto
 export default Botao;
